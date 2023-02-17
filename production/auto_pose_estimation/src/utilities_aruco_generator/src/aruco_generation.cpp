2a026672dd4a1e23579e4c5bd97d7c3e2e15c1cc
hamish-official <hamishcode@gmail.com>
2023-02-17 17:39:04 +0900

    p/c++/utilities ~ aruco_generator 오류코드 수정

--- production/auto_pose_estimation/src/utilities_aruco_generator/src/aruco_generation.cpp
+++ production/auto_pose_estimation/src/utilities_aruco_generator/src/aruco_generation.cpp
@@ -4,12 +4,12 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/aruco.hpp"
 
-static inline void aruco_generator(int width = 4, int height = 5, float marker_size = 0.1f, float marker_seperation_size = 0.1f)
+static inline void aruco_generator(int width = 5, int height = 4, float marker_size = 0.1f, float marker_seperation_size = 0.1f)
 {
   cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
   cv::Ptr<cv::aruco::GridBoard> board = cv::aruco::GridBoard::create(width, height, marker_size, marker_seperation_size, dictionary);
   cv::Mat arucoboard;
-  board->draw(cv::Size(841, 1189), arucoboard, 10, 1);
+  board->draw(cv::Size(1187, 841), arucoboard, 10, 1);
   cv::imwrite("arucoboard.jpg", arucoboard);
 }
 
